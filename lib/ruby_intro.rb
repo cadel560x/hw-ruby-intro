5a98a9c64912f9c2287527bd66952919596b732f
Javier Mantilla <g00329649@gmit.ie>
2019-02-04 21:22:14 +0000

    Resolved

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,55 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+return arr.max(2).reduce(0, :+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+arr.combination(2).any? { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+  return false
+  elsif ( /[a-z]/ =~ s[0].downcase && /[^aeiou]/ =~ s[0].downcase)
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+if (s.to_s =~ /\b[01]+\b/ && s.to_i % 4 == 0)
+return true
+else
+return false
+end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+attr_accessor :isbn
+attr_accessor :price
+
+def initialize(isbn, price)
+raise ArgumentError, 
+"Illegal argument" if isbn.empty?  or price <= 0
+@isbn = isbn
+@price = price
+end
+
+def price_as_string
+sprintf("$%2.2f", @price)
+end
 end
